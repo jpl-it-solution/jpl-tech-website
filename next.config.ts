7b2222efc97a57fe899df28736ddefb8e87383eb
saini-yogendra <yogendra1703s@gmail.com>
2026-01-19 11:59:42 +0530

    Configured deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/jpl-tech-website',
+  assetPrefix: '/jpl-tech-website',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
